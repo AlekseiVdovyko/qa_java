5b8ea87fc8552a19a58e3858226ffc07fd4e0bb5
AlekseiVdovyko <avdovyko@gmail.com>
2024-11-21 18:54:40 +0300

    Удалено лишнее тестирование, изолированы тесты, в зависимостях использованы моки, тестируется результат метода

--- src/test/java/AlexLionTest.java
+++ src/test/java/AlexLionTest.java
@@ -10,30 +10,29 @@ import java.util.List;
 @RunWith(MockitoJUnitRunner.class)
 public class AlexLionTest {
 
-    public AlexLion alexLion = new AlexLion(new Feline());
+    private AlexLion alexLion = new AlexLion(new Feline());;
 
     public AlexLionTest() throws Exception {
     }
 
     @Test
-    public void getValueKittensFromAlexLionPositiveTest() {
-        Assert.assertEquals("У Алекса нет львят", 0, alexLion.getKittens());
-    }
-
-    @Test
-    public void getValueKittensFromAlexLionNegativeTest() {
-        Assert.assertNotEquals("У Алекса нет львят", 1, alexLion.getKittens());
-    }
-
-    @Test
-    public void testingGetLivingAlexLion() {
-        Assert.assertEquals("Ошибка в адресе зоопарка","Нью-Йоркский зоопарк", alexLion.getPlaceOfLiving());
-    }
-
-    @Test
-    public void testingGetFriendsAlexLion() {
-        List<String> actual = List.of("Марти", "Глории", "Мелман");
-        List<String> expected = alexLion.getFriends();
+    public void testingGetFriendsAlexLion() throws Exception {
+        List<String> expected = List.of("Марти", "Глории", "Мелман");
+        List<String> actual = alexLion.getFriends();
         Assert.assertEquals("Ошибка в имени друзей", expected, actual);
+    }
+
+    @Test
+    public void testingGetPlaceOfLivingAlexLion() throws Exception {
+        String expected = "Нью-Йоркский зоопарк";
+        String actual = alexLion.getPlaceOfLiving();
+        Assert.assertEquals("Ошибка в адресе зоопарка",expected, actual);
+    }
+
+    @Test
+    public void getValueKittensFromAlexLionPositiveTest() throws Exception {
+        int expected = 0;
+        int actual = alexLion.getKittens();
+        Assert.assertEquals("У Алекса нет львят", expected, actual);
     }
 }
--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -3,30 +3,32 @@ import com.example.Feline;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Spy;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CatTest {
 
-    @Spy
-    private Cat cat = new Cat(new Feline());
+    @Mock
+    private Feline feline;
 
     @Test
     public void testingMakeSoundAnimal() {
-        Assert.assertEquals("Возвращен неверный результат", "Мяу", cat.getSound());
+        Cat cat = new Cat(feline);
+        String expected = "Мяу";
+        String actual = cat.getSound();
+        Assert.assertEquals("Ошибка в издаваемом звуке животным", expected, actual);
     }
 
     @Test
     public void testingFoodAnimal() throws Exception {
-        List<String> actual = new ArrayList<>();
-        actual.add("Животные");
-        actual.add("Птицы");
-        actual.add("Рыба");
-        List<String> expected = cat.getFood();
+        Cat cat = new Cat(feline);
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        List<String> actual = cat.getFood();
         Assert.assertEquals("Ошибка в названии животных", expected, actual);
     }
 }
